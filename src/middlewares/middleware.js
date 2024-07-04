92c4e447bc05d9a1cdccbeeb2f9aa8644649e812
Matheus Vasco <matheus.vasco@carmehil.com.br>
2024-07-04 17:32:51 -0300

    Configuração inclusão de contatos

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -12,12 +12,21 @@ exports.outroMiddleware = (req, res, next) => {
 
 exports.checkCsrfError = (err, req, res, next) => {
     if(err) {
-        console.log("Passei aqui 12121");
         return res.render('404')
     }
 }
 
 exports.csrfMiddleware = (req, res, next) => {
     res.locals.csrfToken = req.csrfToken();
+    next();
+}
+
+exports.loginRequired = (req, res, next) => {
+    if(!req.session.user) {
+        req.flash('errors', 'Você precisa fazer login.');
+        req.session.save(() => res.redirect('/'));
+        return;
+    }
+
     next();
 }
